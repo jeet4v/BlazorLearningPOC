f9bd0bbc7261f4e69f92a134986ee38829888e3d
Jitendra Singh <jeet4v25@gmail.com>
2025-04-10 17:37:08 +0530

    Committing the changes

---
+++ Components/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorLearningPOC.Components.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<ProductProp> ProductProperties { get; set; }
+
+    }
+}
---
+++ Components/Models/ProductProp.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorLearningPOC.Components.Model
+{
+    public class ProductProp
+    {
+        public int ID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
