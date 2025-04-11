65329e3f3cecc6c67beb9acba0c4da5c2e8d1b86
Jitendra Singh <jeet4v25@gmail.com>
2025-04-11 18:33:17 +0530

    commiting the changes

---
+++ Components/Helpers/IJSRuntimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorLearningPOC.Components.Helpers
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string strMessage)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "success", strMessage);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jsRuntime, string strMessage)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "error", strMessage);
+        }
+
+    }
+}
---
+++ wwwroot/lib/js/common.js
@@ -0,0 +1,28 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
+
+window.showSweetalert = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: "Success",
+            text: message,
+            icon: "question"
+        });
+    }
+
+    if (type == "error") {
+        Swal.fire({
+            title: "Error",
+            text: message,
+            icon: "error"
+        });
+    }
+}
+
