fe1328ab4e4ae577763a2da607d1614dd3e9a94e
Jitendra Singh <jeet4v25@gmail.com>
2025-04-14 19:57:17 +0530

    Comitting the changes . . .

--- Components/Models/Product.cs
+++ Components/Models/Product.cs
@@ -1,12 +1,32 @@
-﻿namespace BlazorLearningPOC.Components.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorLearningPOC.Components.Model
 {
     public class Product
     {
+        public Product()
+        {
+            AvailableAfter = DateOnly.FromDateTime(DateTime.Now);
+        }
         public int ID { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+        [Range(1, 1000)]
         public double Price { get; set; }
         public bool IsActive { get; set; }
         public List<ProductProp> ProductProperties { get; set; }
 
+        public Category Category { get; set; }
+        public DateOnly AvailableAfter { get; set; }
+
     }
+
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert
+    }
+
 }
